b53ce86fd054fa2da8fca62af3bcece1651d6529
Karim Luy <karim.luy@he-arc.ch>
2017-03-24 17:29:30 +0100

    finalized the db downloader

--- gavageDeDB.py
+++ gavageDeDB.py
@@ -2,9 +2,10 @@ import re
 import urllib
 import os
 from urllib import request
+import sys
 
 
-def download_icon(link, directory, sex, character):
+def download_item_icon(link, directory, sex, character):
     filename = link.replace('demonhunter_male', character + '_' + sex).split('/')[-1]
     print('checking: ' + filename)
     if os.path.isfile(directory + filename):
@@ -12,18 +13,36 @@ def download_icon(link, directory, sex, character):
         return
     try:
         print('downloading...')
-        urllib.request.urlretrieve(link.replace('demonhunter_male', character + '_' + sex), directory + filename)
+        if character == 'crusader':
+            urllib.request.urlretrieve(link.replace('demonhunter_male', 'x1_' + character + '_' + sex),
+                                       directory + filename)
+        else:
+            urllib.request.urlretrieve(link.replace('demonhunter_male', character + '_' + sex), directory + filename)
         print(filename + ' downloaded!')
     except urllib.error.URLError:
         print('remote file doesn\'t exist, skipping')
         pass
-    except:
-        print('download failed, skipping')
-        pass
+        # except:
+        #     print('download failed, skipping')
+        #     pass
 
 
-def get_links(source, item, download=False):
-    # for now catches only male legendary and set items												[A-Z_\:0-9a-z]*
+def download_spell_icon(link, directory):
+    filename = link.split('/')[-1]
+    if 'male' not in filename:
+        if os.path.isfile(directory + filename):
+            print('\tfile exists, skipping')
+            return
+        try:
+            print('downloading...')
+            print(link)
+            urllib.request.urlretrieve(link, directory + filename)
+            print(filename + ' downloaded!')
+        except urllib.error.URLError:
+            print('remote file doesn\'t exist, skipping')
+
+
+def get_item_links(source, item, download=False):
     for link in re.findall(
             '[\bhttps://blzmedia\-a.akamaihd.net/d3/icons/items/large/\b]+[A-Z_\:0-9a-z]*[0-9]{2,50}[A-Z_\:0-9a-z]*.png',
             source):
@@ -32,57 +51,54 @@ def get_links(source, item, download=False):
             if not os.path.exists(directory):
                 os.makedirs(directory)
             for character in characters:
-                download_icon(link, directory, 'male', character)
-                download_icon(link, directory, 'female', character)
+                download_item_icon(link, directory, 'male', character)
+                download_item_icon(link, directory, 'female', character)
+        else:
+            print(link)
+
+
+def get_class_links(source, character, download=False):
+    for link in re.findall('[\bhttps://blzmedia\-a.akamaihd.net/d3/icons/skills/64/\b]+[a-z_0-9]*_[a-z]*.png',source):
+        if download:
+            dir = os.getcwd() + '\\database\\characters\\' + character + '-spells\\'
+            if not os.path.exists(dir):
+                os.makedirs(dir)
+            download_spell_icon(link, dir)
         else:
             print(link)
 
 
 if __name__ == '__main__':
+    args = sys.argv
     characters = ['barbarian', 'crusader', 'demonhunter', 'monk', 'wizard', 'witchdoctor']
-    items = ['helm',
-             'spirit-stone',
-             'voodoo-mask',
-             'wizard-hat',
-             'pauldrons',
-             'chest-armor',
-             'cloak',
-             'bracers',
-             'gloves',
-             'belt',
-             'mighty-belt',
-             'pants',
-             'boots',
-             'amulet',
-             'ring',
-             'shield',
-             'crusader-shield',
-             'mojo',
-             'orb',
-             'quiver',
-             'axe-1h',
-             'dagger',
-             'mace-1h',
-             'spear',
-             'sword-1h',
-             'ceremonial-knife',
-             'fist-weapon',
-             'flail-1h',
-             'mighty-weapon-1h',
-             'axe-2h',
-             'mace-2h',
-             'polearm',
-             'staff',
-             'sword-2h',
-             'daibo',
-             'flail-2h',
-             'mighty-weapon-2h',
-             'bow',
-             'crossbow',
-             'hand-crossbow',
-             'wand'
-             ]
-    for item in items:
-        get_links(urllib.request.urlopen(
-            'https://us.battle.net/d3/en/item/' + item + '/#type=legendary&').read().decode('utf-8'),
-                  item, True)
+    items = ['helm', 'spirit-stone', 'voodoo-mask', 'wizard-hat', 'pauldrons', 'chest-armor', 'cloak', 'bracers',
+             'gloves', 'belt', 'mighty-belt', 'pants', 'boots', 'amulet',
+             'ring', 'shield', 'crusader-shield', 'mojo', 'orb', 'quiver', 'axe-1h', 'dagger', 'mace-1h', 'spear',
+             'sword-1h', 'ceremonial-knife', 'fist-weapon', 'flail-1h',
+             'mighty-weapon-1h', 'axe-2h', 'mace-2h', 'polearm', 'staff', 'sword-2h', 'daibo', 'flail-2h',
+             'mighty-weapon-2h', 'bow', 'crossbow', 'hand-crossbow', 'wand']
+    if 'items' in args:
+        for item in items:
+            get_item_links(urllib.request.urlopen(
+                'https://us.battle.net/d3/en/item/' + item).read().decode('utf-8'), item, True)
+    if 'chars' in args:
+        for char in characters:
+            dir = os.getcwd() + '\\database\\characters\\'
+            if not os.path.exists(dir):
+                os.makedirs(dir)
+            download_item_icon('https://blzmedia-a.akamaihd.net/d3/icons/portraits/42/demonhunter_male.png',
+                               dir, 'male', char)
+            download_item_icon('https://blzmedia-a.akamaihd.net/d3/icons/portraits/42/demonhunter_male.png',
+                               dir, 'female', char)
+    if 'spells' in args:
+        for char in characters:
+            if char == 'demonhunter':
+                char = 'demon-hunter'
+            if char == 'witchdoctor':
+                char = 'witch-doctor'
+            get_class_links(urllib.request.urlopen(
+                'https://us.battle.net/d3/en/class/' + char + '/active/').read().decode('utf-8'), char, True)
+            get_class_links(urllib.request.urlopen(
+                'https://us.battle.net/d3/en/class/' + char + '/passive/').read().decode('utf-8'), char, True)
+    if len(args) == 1:
+        print('Please insert one of the following arguements: spells, items, or chars')
